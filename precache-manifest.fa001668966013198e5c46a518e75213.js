78258af0b232812e1cb369bb551aa6a0ebec1744
David <bandadavid531@gmail.com>
2020-08-15 11:07:22 -0500

    Updates

---
+++ precache-manifest.fa001668966013198e5c46a518e75213.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad7311b56b65ce0180b031e003879e59",
+    "url": "/CoronavirusTiempoReal/index.html"
+  },
+  {
+    "revision": "f2be7d529a7f59f6f137",
+    "url": "/CoronavirusTiempoReal/static/css/main.7a8df756.chunk.css"
+  },
+  {
+    "revision": "89796057f360637e633d",
+    "url": "/CoronavirusTiempoReal/static/js/2.6e833e34.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/CoronavirusTiempoReal/static/js/2.6e833e34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2be7d529a7f59f6f137",
+    "url": "/CoronavirusTiempoReal/static/js/main.c1aa67d3.chunk.js"
+  },
+  {
+    "revision": "86fca16ddc90e4c6ff3b",
+    "url": "/CoronavirusTiempoReal/static/js/runtime-main.7a6e0fe0.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/CoronavirusTiempoReal/static/media/image.d7265326.png"
+  }
+]);
